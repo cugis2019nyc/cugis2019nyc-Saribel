ff8d8eb732c736ce54b8f7c998cff328636905f7
Saribel <54039852+Saribel@users.noreply.github.com>
2019-08-13 13:43:24 -0400

    Add files via upload

---
+++ CUGIS Coding, Python Day 1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+print("Hello World") #greeting
+print("My name is Saribel")
+print(6) 
+5*2
+5/2
+5-2
+5**2
+8/9*3
+def add(a,b):
+    add= a+b
+    print(add)
+add(4,9)
+
+def division(a,b):
+    division= a/b
+    print ("the answer is", division)
+division(2,5)
+def triangle(b,h):
+    triangle= 1/2*b*h
+    print("the area of the triangle with the base and height of", b,h, "would be", triangle)
+triangle(4,6)
+    
+    

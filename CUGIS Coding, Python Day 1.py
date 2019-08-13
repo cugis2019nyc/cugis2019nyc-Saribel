7a8e4c7332006216626535e8426d091ce36a6a72
Saribel <54039852+Saribel@users.noreply.github.com>
2019-08-13 15:00:39 -0400

    Add files via upload

--- CUGIS Coding, Python Day 1.py
+++ CUGIS Coding, Python Day 1.py
@@ -25,5 +25,24 @@ def triangle(b,h):
     triangle= 1/2*b*h
     print("the area of the triangle with the base and height of", b,h, "would be", triangle)
 triangle(4,6)
-    
-    
+chocolate1 = "milk"
+chocolate2 = "white"
+chocolate3 = "dark"
+milk = 5
+white = 8
+dark = 6
+
+milk= milk+3
+milk
+milk=10
+milk
+import math
+dir(math)
+math.factorial(7)
+math.sqrt(350)
+math.exp(5)
+math.exp(1)
+math.pow(2.718281828459045,2)
+math.pow(35,9)
+35**9
+math.pi
